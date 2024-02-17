d455c93ef342d9df0bd69c4f1a390840602ff38e
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-02-17 12:30:17 +0300

    Register api_v1_router

---
+++ app/src/api/router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from src.api.v1.router import router as api_v1_router
+
+router = APIRouter()
+
+router.include_router(
+    router=api_v1_router,
+    prefix='/v1',
+    tags=['v1'],
+)
