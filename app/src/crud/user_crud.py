d0c0eed06e623a82d1657c6bcfac157db5c59d2b
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-02-19 05:49:33 +0300

    Add CrudUser

---
+++ app/src/crud/user_crud.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from src.crud.base_crud import BaseAsyncCrud
+from src.models.user import User
+from src.schemas.user import UserCreate, UserUpdate
+
+
+class CrudUser(BaseAsyncCrud[User, UserCreate, UserUpdate]):
+
+    async def retrieve_by_email(
+        self,
+        *,
+        session: AsyncSession,
+        email: str
+    ) -> Optional[User]:
+        query = select(self.model).where(self.model.email == email)
+        result = await session.execute(query)
+        return result.scalars().first()
+
+
+crud_user = CrudUser(User)
