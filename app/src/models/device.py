55f665fbe3ee8d31abf883362d8233ff465b2ebb
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-02-19 05:49:57 +0300

    Add Device

---
+++ app/src/models/device.py
@@ -0,0 +1,37 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database.database import Base
+
+if TYPE_CHECKING:
+    from src.models.user import User
+
+
+class Device(Base):
+    __tablename__ = 'table_device'
+
+    id: Mapped[int] = mapped_column(
+        comment='id устройства',
+        index=True,
+        primary_key=True,
+    )
+    serial: Mapped[str] = mapped_column(
+        String,
+        comment='Серийный номер устройства',
+        unique=True,
+    )
+
+    user: Mapped['User'] = relationship(
+        'User',
+        back_populates='device',
+        single_parent=True,
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            'table_user.id',
+            ondelete='CASCADE',
+        ),
+        comment='Пользователь устройства',
+    )
